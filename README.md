f0009cb1f41511ccaeae4b52dccb54cdab1dbc2a
Alvin Zhang <zhangjiawen@ycfis.com>
2016-08-01 15:56:36 +0800

    初始化，呵呵。

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # spring-boot-helloworld
 spring-boot-helloworld
+----
+ref: http://jinnianshilongnian.iteye.com/blog/1997192
+ref: http://blog.csdn.net/catoop/article/details/50588851
+----
+application.properties
+----
+# 服务端口
+server.port=9090
+----
+java -jar spring-boot-helloworld-1.0-SNAPSHOT.jar --spring.config.location=~/application.properties
+----
+http://localhost:8080/user/1
+http://10.0.250.222:9090/user/1
---
+++ src/main/java/com/gmail/mosoft521/controller/UserController.java
@@ -0,0 +1,31 @@
+package com.gmail.mosoft521.controller;
+
+import com.gmail.mosoft521.entity.User;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>User: Zhang Kaitao
+ * <p>Date: 13-12-22
+ * <p>Version: 1.0
+ */
+//@EnableAutoConfiguration
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+//    public static void main(String[] args) {
+//        SpringApplication.run(UserController.class);
+//    }
+
+    @RequestMapping("/{id}")
+    private User view(@PathVariable("id") Long id) {
+        User user = new User();
+        user.setId(id);
+        user.setName("zhang");
+        return user;
+    }
+}
